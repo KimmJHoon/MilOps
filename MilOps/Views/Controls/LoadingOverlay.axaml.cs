45e5afb4d02c4b7ef1093c36c7aa316be4ecc11b
goldJeong <manage06@naver.com>
2026-02-01 08:55:10 +0900

    chore: 앱 설정 업데이트 및 로딩 오버레이 컨트롤 추가

---
+++ MilOps/Views/Controls/LoadingOverlay.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MilOps.Views.Controls;
+
+public partial class LoadingOverlay : UserControl
+{
+    public LoadingOverlay()
+    {
+        InitializeComponent();
+    }
+}
